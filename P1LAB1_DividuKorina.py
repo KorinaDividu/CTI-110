9ae3afd268d58d4c35f4536e607bcd43f07441c7
KorinaDividu <dividuk8999@student.faytechcc.edu>
2026-06-13 14:18:24 -0400

    First python assignment
    Taking input from user and printing output

---
+++ P1LAB1_DividuKorina.py
@@ -0,0 +1,9 @@
+#Korina Dividu
+#06/14/2026
+#p1lab1_DividuKorina.py
+#input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110!")
