3fb05f4668f018f4ab00b705dc6ad37ec5bc5b59
KorinaDividu <dividuk8999@student.faytechcc.edu>
2026-07-01 17:55:57 -0400

    Dollars/coins needed for a given amount of money
    This program calculates the most efficient number of dollars, quarters, dimes, nickels, and pennies needed to make the given amount of money

---
+++ P3LAB_DividuKorina.py
@@ -0,0 +1,60 @@
+#Korina Dividu
+#06/28/2026
+#P3LAB_DividuKorina.py
+#This program calculates the most efficient number of dollars, quarters, dimes, nickels, and pennies needed to make the given amount of money
+
+#Get value from user
+change = float(input("Enter the amount of money as a float: $"))
+
+#Converting the value to an integer
+change = round(change * 100)
+
+#Determine how many coins are needed
+num_dollars = change // 100
+change = change - (num_dollars * 100)
+
+num_quarters = change // 25
+change = change - (num_quarters * 25)
+
+num_dimes = change // 10
+change = change - (num_dimes * 10)
+
+num_nickels = change // 5
+change = change - (num_nickels * 5)
+
+num_pennies = change 
+
+if change == 0.00:
+        print("No change")
+else:
+    change = round(change * 100)
+
+if num_dollars > 0:
+    if num_dollars ==1:
+        print(f"{num_dollars} Dollar")
+    else:
+        print(f"{num_dollars} Dollars")
+        
+if num_quarters > 0:
+    if num_quarters ==1:
+        print(f"{num_quarters} Quarter")
+    else:
+        print(f"{num_quarters} Quarters")
+
+if num_dimes > 0:
+    if num_dimes ==1:
+        print(f"{num_dimes} Dime")
+    else:
+        print(f"{num_dimes} Dimes")
+
+if num_nickels > 0:
+    if num_nickels ==1:
+        print(f"{num_nickels} Nickel")
+    else:
+        print(f"{num_nickels} Nickels")
+
+if num_pennies > 0:
+    if num_pennies ==1:
+        print(f"{num_pennies} Penny")
+    else:
+        print(f"{num_pennies} Pennies")
